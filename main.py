f89f6e069f62e9c843652a19813e4731c04fb6e5
jzm <canesfanjake1@gmail.com>
2026-05-19 21:08:53 -0400

    added Line and Point classes, along with methods to draw a line between two points

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-from graphics import Window
+from graphics import Window, Point, Line
 
 def main():
     app_window = Window(800, 600)
+
+    point1 =  Point(10, 30)
+    point2 = Point(40, 70)
+
+    line = Line(point1, point2)
+
+    app_window.draw_line(line, "black")
+
     app_window.wait_for_close()
 
 if __name__ == "__main__":
